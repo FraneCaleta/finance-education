f0a7926bafb1afa3c5f7e04fbde22d173fa617cb
Frane Caleta <frane.caleta@yahoo.com>
2025-03-26 21:23:14 +0100

    make it responsive on smaller devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,15 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         className={`${geistSans.className} ${geistMono.className} antialiased min-h-screen flex flex-col`}
       >
         <header className="bg-gray-800 text-white p-4">
-          <div className="container mx-auto">
+          <div className="container mx-auto px-4 md:px-0">
             <Navigation />
           </div>
         </header>
-        <main className="flex-grow container mx-auto py-6">{children}</main>
+        <main className="flex-grow">
+          <div className="container mx-auto px-6 md:px-4 py-8">{children}</div>
+        </main>
         <footer className="bg-gray-800 text-white py-6">
-          <div className="container mx-auto text-center">
+          <div className="container mx-auto px-4 md:px-0 text-center">
             <p>© 2023 My Finance App. All rights reserved.</p>
             <p>
               Follow us on{" "}
